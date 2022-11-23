c630e822031418619271dfdd6b48e4fda6ac0cc7
daniel-valean <dannyvalean@gmail.com>
2022-11-23 16:01:19 -0700

    login and signup code

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,10 @@ import {
   createHttpLink,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage
   const token = localStorage.getItem('id_token');
@@ -19,9 +23,6 @@ const authLink = setContext((_, { headers }) => {
       authorization: token ? `Bearer ${token}` : '',
     },
   };
-});
-const httpLink = createHttpLink({
-  uri: '/graphql',
 });
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
